2de5093437b592819929b7704bc9758d023eed9f
aleksanderbrymora <aleksander.australia@gmail.com>
2019-11-19 23:39:44 +1100

    Cleaned Flights.js a bit

--- src/components/home/Flights.js
+++ src/components/home/Flights.js
@@ -1,21 +1,14 @@
 import React from 'react'
 
 const Flights = (props) => {
-	console.log(props);
-	console.log((!!props.flights));
-
 	if (!!props.flights) {
 		return (
 			<div>
 				<h5>Available flights</h5>
-				{props.flights.map((data) => <p> {data.origin} -> {data.destination} - {data.date} </p>)}
+				{props.flights.map((data) => <p key={data.id}> {data.origin} -> {data.destination} - {data.date} </p>)}
 			</div>
 		);
-	} else {
-		return (
-			<div></div>
-		)
-	}
+	} else return (null)
 }
 
 export default Flights;
