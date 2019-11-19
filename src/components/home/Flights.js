df00aae248fd6028f89f545f4b7d74a01ed4026c
aleksanderbrymora <aleksander.australia@gmail.com>
2019-11-20 10:18:33 +1100

    finished listing flights

--- src/components/home/Flights.js
+++ src/components/home/Flights.js
@@ -1,14 +1,37 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Flight = (props) => {
+	const url = `/flight/${props.id}`
+	return (
+		<p alt={props.id}>
+			{props.origin} -> {props.destination} - {props.date} <Link to={url}>Select</Link>
+		</p>
+	)
+}
 
 const Flights = (props) => {
 	if (!!props.flights) {
 		return (
 			<div>
 				<h5>Available flights</h5>
-				{props.flights.map((data) => <p key={data.id}> {data.origin} -> {data.destination} - {data.date} </p>)}
+				<div>
+					{props.flights.map((data) => {
+						return (
+							<Flight 
+								key={data.id}
+								id={data.id}
+								origin={data.origin}
+								destination={data.destination}
+							/>
+						)
+					})}
+				</div>
 			</div>
 		);
-	} else return (null)
+	} else {
+		return (null);
+	}
 }
 
 export default Flights;
